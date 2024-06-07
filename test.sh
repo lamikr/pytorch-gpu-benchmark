c221806844dd72f303282f1675b7aff556c4f502
Mika Laitio <lamikr@gmail.com>
2024-06-07 11:48:32 -0700

    Add support for running AMD GPU benchmarks
    - AMD GPU benchmarks works on rocm platform
      (tested on rocm 6.1.1) and offers support
      of using CUDA api with AMD gpus
    - only code change is needed is to test.sh
      to check whether rocm-smi tool is available
      and whether it returns AMD GPUs
    - rocm-smi can be used to query the AMD gpu count
    - I had pytorch 2.3.0 and pytorch vision 0.18.0 installed
      with AMD rocm support enabled and all tests executed
      without errors on AMD RX 6800
    
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- test.sh
+++ test.sh
@@ -1,9 +1,25 @@
 #! /bin/bash
-count=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
 
-echo 'start'
-for (( c=$count; c>=1; c-- ))
-do
-      python3 benchmark_models.py -g $c&& &>/dev/null 
-done
-echo 'end'
+if [ -x "$(command -v rocm-smi)" ]; then
+    count=`rocm-smi --showproductname --json | wc -l`
+
+    echo 'AMD gpu benchmarks starting'
+    echo "GPU count: " ${count}
+    for (( c=$count; c>=1; c-- ))
+    do
+        python3 benchmark_models.py -g $c&& &>/dev/null
+    done
+    echo 'AMD GPU benchmarks finished'
+fi
+
+if [ -x "$(command -v nvidia-smi)" ]; then
+    count=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
+
+    echo 'NVidia gpu benchmarks starting'
+    echo "GPU count: " ${count}
+    for (( c=$count; c>=1; c-- ))
+    do
+        python3 benchmark_models.py -g $c&& &>/dev/null
+    done
+    echo 'Nvidia GPU benchmarks finished'
+fi
