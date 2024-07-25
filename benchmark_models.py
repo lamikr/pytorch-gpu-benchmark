67aa35ef0cf9d227d5e344b79e1374e80eeec3a2
Mika Laitio <lamikr@gmail.com>
2024-07-24 22:26:39 -0700

    use gpu memory to select benchmark models
    - defined minimal, medium and full set of models
    - use the amount of gpu memory as a criteria
      whether to run minimal. medium or full set of models
      (8GB device can run medium set without getting
       out of memory error)
    
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- benchmark_models.py
+++ benchmark_models.py
@@ -18,13 +18,53 @@ torch.backends.cudnn.benchmark = True
 # This flag allows you to enable the inbuilt cudnn auto-tuner to find the best algorithm to use for your hardware.
 # If you check it using the profile tool, the cnn method such as winograd, fft, etc. is used for the first iteration and the best operation is selected for the device.
 
-MODEL_LIST = {
+# mnasnet0_5,mnasnet0_75
+# resnet18,resnet34,resnet50
+# densenet121,
+# squeezenet1_0,
+# vgg11,vgg11_bn,
+# mobilenet_v3_small,
+# shufflenet_v2_x0_5,shufflenet_v2_x1_0, shufflenet_v2_x1_5,shufflenet_v2_x2_0
+MODEL_LIST_MINIMAL = {
+    models.mnasnet: ["mnasnet0_5", "mnasnet0_75"],
+    models.resnet: ["resnet18", "resnet34", "resnet50"],
+    models.densenet: ["densenet121"],
+    models.squeezenet: ["squeezenet1_0"],
+    models.vgg: ["vgg11", "vgg11_bn"],
+    models.mobilenet: ["mobilenet_v3_small"],
+    models.shufflenetv2: ["shufflenet_v2_x0_5"],
+}
+
+# mnasnet0_5,mnasnet0_75,mnasnet1_0,mnasnet1_3
+# resnet18,resnet34,resnet50,resnet101,resnet152,resnext50_32x4d,resnext101_32x8d,resnext101_64x4d,wide_resnet50_2,wide_resnet101_2,
+# densenet121,densenet161,densenet169,densenet201
+# squeezenet1_0,squeezenet1_1,
+# vgg11,vgg11_bn,vgg13,vgg13_bn,vgg16,vgg16_bn,vgg19,vgg19_bn,
+# mobilenet_v3_large,mobilenet_v3_small,
+# shufflenet_v2_x0_5,shufflenet_v2_x1_0, shufflenet_v2_x1_5,shufflenet_v2_x2_0
+MODEL_LIST_MEDIUM = {
+    models.mnasnet: ["mnasnet0_5", "mnasnet0_75", "mnasnet1_0"],
+    models.resnet: ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152"],
+    models.densenet: ["densenet121", "densenet161"],
+    models.squeezenet: ["squeezenet1_0", "squeezenet1_1"],
+    models.vgg: ["vgg11", "vgg11_bn", "vgg13", "vgg13_bn", "vgg16", "vgg16_bn"],
+    models.mobilenet: ["mobilenet_v3_large", "mobilenet_v3_small"],
+    models.shufflenetv2: ["shufflenet_v2_x0_5", "shufflenet_v2_x1_5"],
+}
+
+# mnasnet0_5,mnasnet0_75,mnasnet1_0,mnasnet1_3
+# resnet18,resnet34,resnet50,resnet101,resnet152,resnext50_32x4d,resnext101_32x8d,resnext101_64x4d,wide_resnet50_2,wide_resnet101_2,
+# densenet121,densenet161,densenet169,densenet201
+# squeezenet1_0,squeezenet1_1,
+# vgg11,vgg11_bn,vgg13,vgg13_bn,vgg16,vgg16_bn,vgg19,vgg19_bn,
+# mobilenet_v3_large,mobilenet_v3_small,
+# shufflenet_v2_x0_5,shufflenet_v2_x1_0, shufflenet_v2_x1_5,shufflenet_v2_x2_0
+MODEL_LIST_FULL = {
     models.mnasnet: models.mnasnet.__all__[1:],
     models.resnet: models.resnet.__all__[1:],
     models.densenet: models.densenet.__all__[1:],
     models.squeezenet: models.squeezenet.__all__[1:],
     models.vgg: models.vgg.__all__[1:],
-    models.mobilenet: models.mobilenet.mv2_all[1:],
     models.mobilenet: models.mobilenet.mv3_all[1:],
     models.shufflenetv2: models.shufflenetv2.__all__[1:],
 }
@@ -78,24 +118,28 @@ class RandomDataset(Dataset):
     def __len__(self):
         return self.len
 
-def train(precision="single", gpu_index=-1):
+def train(precision="single", gpu_index=-1, benchmark_model=MODEL_LIST_MINIMAL):
     """use fake image for training speed test"""
-    target = torch.LongTensor(args.BATCH_SIZE).random_(args.NUM_CLASSES).cuda()
+    if gpu_index >= 0:
+        target = torch.LongTensor(args.BATCH_SIZE).random_(args.NUM_CLASSES).cuda(gpu_index)
+    else:
+        target = torch.LongTensor(args.BATCH_SIZE).random_(args.NUM_CLASSES).cuda()
     criterion = nn.CrossEntropyLoss()
     benchmark = {}
-    for model_type in MODEL_LIST.keys():
-        for model_name in MODEL_LIST[model_type]:
+    for model_type in benchmark_model.keys():
+        for model_name in benchmark_model[model_type]:
             if model_name[-8:] == '_Weights': continue
+            torch_device_name = "cuda"
+            if (gpu_index >= 0):
+                torch_device_name = "cuda:" + str(gpu_index)
+                torch.cuda.set_device(gpu_index)
+            print("torch_device_name: " + torch_device_name)
             model = getattr(model_type, model_name)()
             if args.GPU_COUNT > 1:
                 model = nn.DataParallel(model, device_ids=range(args.GPU_COUNT))
             model = getattr(model, precision)()
-            torch_device_name = "cuda"
-            if (gpu_index >= 0):
-                torch_device_name = "cuda:" + str(gpu_index)
-            print("torch_device_name: " + torch_device_name)
             torch_device = torch.device(torch_device_name)
-            model = model.to(torch_device)        
+            model = model.to(torch_device)
             durations = []
             print(f"Benchmarking Training {precision} precision type {model_name} ")
             for step, img in enumerate(rand_loader):
@@ -114,24 +158,27 @@ def train(precision="single", gpu_index=-1):
                 f"{model_name} model average train time : {sum(durations)/len(durations)}ms"
             )
             del model
+            torch.cuda.empty_cache()
             benchmark[model_name] = durations
+            print(torch.cuda.memory_summary())
     return benchmark
 
-def inference(precision="float", gpu_index=-1):
+def inference(precision="float", gpu_index=-1, benchmark_model=MODEL_LIST_MINIMAL):
     benchmark = {}
     with torch.no_grad():
-        for model_type in MODEL_LIST.keys():
-            for model_name in MODEL_LIST[model_type]:
+        for model_type in benchmark_model.keys():
+            for model_name in benchmark_model[model_type]:
                 if model_name[-8:] == '_Weights': continue
+                torch_device_name = "cuda"
+                if (gpu_index >= 0):
+                    torch_device_name = "cuda:" + str(gpu_index)
+                    torch.cuda.set_device(gpu_index)
+                print("torch_device_name: " + torch_device_name)
+                torch_device = torch.device(torch_device_name)
                 model = getattr(model_type, model_name)()
                 if args.GPU_COUNT > 1:
                     model = nn.DataParallel(model, device_ids=range(args.GPU_COUNT))
                 model = getattr(model, precision)()
-                torch_device_name = "cuda"
-                if (gpu_index >= 0):
-                    torch_device_name = "cuda:" + str(gpu_index)
-                print("torch_device_name: " + torch_device_name)
-                torch_device = torch.device(torch_device_name)
                 model = model.to(torch_device)
                 model.eval()
                 durations = []
@@ -142,7 +189,7 @@ def inference(precision="float", gpu_index=-1):
                     img = getattr(img, precision)()
                     torch.cuda.synchronize()
                     start = time.time()
-                    model(img.to("cuda"))
+                    model(img.to(torch_device))
                     torch.cuda.synchronize()
                     end = time.time()
                     if step >= args.WARM_UP:
@@ -151,7 +198,9 @@ def inference(precision="float", gpu_index=-1):
                     f"{model_name} model average inference time : {sum(durations)/len(durations)}ms"
                 )
                 del model
+                torch.cuda.empty_cache()
                 benchmark[model_name] = durations
+                print(torch.cuda.memory_summary())
     return benchmark
 
 f"{platform.uname()}\n{psutil.cpu_freq()}\ncpu_count: {psutil.cpu_count()}\nmemory_available: {psutil.virtual_memory().available}"
@@ -173,10 +222,45 @@ if __name__ == "__main__":
     print("gpu_index: " + str(gpu_index))
     print("gpu_count: " + str(gpu_count))
 
+    gpu_mem_total = 0
+    gpu_mem_used = 0
     if (gpu_index >= 0):
         device_name = str(torch.cuda.get_device_name(gpu_index))
+        mem_tuple = torch.cuda.mem_get_info(gpu_index)
+        gpu_mem_total = mem_tuple[0]
+        gpu_mem_free = mem_tuple[1]
     else:
         device_name = str(torch.cuda.get_device_name(0))
+        # search which gpu has smallest amount of memory
+        for ii in range (gpu_count):
+            mem_tuple = torch.cuda.mem_get_info(ii)
+            if (ii == 0):
+                gpu_mem_total = mem_tuple[0]
+                gpu_mem_free = mem_tuple[1]
+            else:
+                if (mem_tuple[0] < gpu_mem_total):
+                    gpu_mem_total = mem_tuple[0]
+                    gpu_mem_free = mem_tuple[1]
+
+    # convert memory sizes to gigabytes (gb)
+    gpu_mem_total = gpu_mem_total / (1048576.0 * 1024)
+    gpu_mem_free = gpu_mem_free / (1048576.0 * 1024)
+    gpu_mem_used = gpu_mem_total - gpu_mem_free
+    # 3 different set of benchmarks depending of the gpu memory available
+    # to avoid out of memory errors (failed to allocate memory error in pytorch)
+    gpu_benchmark_set = 0
+    gpu_benchmark_set_desc = "minimal"
+    benchmark_model = MODEL_LIST_MINIMAL
+    if (gpu_mem_free >= 6) and (gpu_mem_free <= 10):
+        gpu_benchmark_set = 1
+        gpu_benchmark_set_desc = "medium"
+        benchmark_model = MODEL_LIST_MEDIUM
+    else:
+        if (gpu_mem_free > 10):
+            gpu_benchmark_set = 2
+            gpu_benchmark_set_desc = "full"
+            benchmark_model = MODEL_LIST_FULL
+    print("mem_type: " + gpu_benchmark_set_desc + ", mem free: " + str(gpu_mem_free))
     device_name = f"{device_name}"
     if (args.GPU_COUNT > 1):
         device_name = device_name + str(gpu_count) + "X"
@@ -191,9 +275,10 @@ if __name__ == "__main__":
     print("folder_name: " + folder_name)
 
     system_configs = f"{platform.uname()}\n\
-                     {psutil.cpu_freq()}\n\
+                    {psutil.cpu_freq()}\n\
                     cpu_count: {psutil.cpu_count()}\n\
-                    memory_available: {psutil.virtual_memory().available}"
+                    memory_available: {psutil.virtual_memory().available}\n\
+                    gpu_benchmark_set: {gpu_benchmark_set_desc}"
     gpu_configs = [
         gpu_count,
         torch.__version__,
@@ -201,6 +286,9 @@ if __name__ == "__main__":
         torch.version.cuda,
         torch.backends.cudnn.version(),
         device_name,
+        gpu_mem_total,
+        gpu_mem_used,
+        gpu_benchmark_set_desc,
     ]
     gpu_configs = list(map(str, gpu_configs))
     temp = [
@@ -210,6 +298,9 @@ if __name__ == "__main__":
         "CUDA_Version: ",
         "Cudnn_Version: ",
         "Device_Name: ",
+        "GPU_Mem_Total_GB: ",
+        "GPU_Mem_Free_GB: ",
+        "Benchmark_Model_Size: ",
     ]
 
     os.makedirs(folder_name, exist_ok=True)
@@ -234,12 +325,12 @@ if __name__ == "__main__":
         f.writelines(s + "\n" for s in gpu_configs)
 
     for precision in precisions:
-        train_result = train(precision, gpu_index)
+        train_result = train(precision, gpu_index, benchmark_model)
         train_result_df = pd.DataFrame(train_result)
         path = f"{folder_name}/{device_file_name}_{precision}_model_train_benchmark.csv"
         train_result_df.to_csv(path, index=False)
 
-        inference_result = inference(precision, gpu_index)
+        inference_result = inference(precision, gpu_index, benchmark_model)
         inference_result_df = pd.DataFrame(inference_result)
         path = f"{folder_name}/{device_file_name}_{precision}_model_inference_benchmark.csv"
         inference_result_df.to_csv(path, index=False)
