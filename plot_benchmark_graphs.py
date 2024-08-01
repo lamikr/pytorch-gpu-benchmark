2dee76875f71c5b433136099ff6d46c8ef271669
Mika Laitio <lamikr@pilppa.org>
2024-08-01 15:21:42 -0700

    initial benchmark result plotter
    - datafiles to be plotted must still be
      hardcoded in the code as a list
    - can show graphs that can be used to compare
      results from multiple amd and nvidia gpus
    - problem when showing the results is that
      the benchmarks that have been run on different
      GPU's have varied during the time. The list of
      benchmarks in full set is nowadays much bigger
      than on rx 1080/2060 release time.
    
    Signed-off-by: Mika Laitio <lamikr@pilppa.org>

---
+++ plot_benchmark_graphs.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def substring_before(s, delim):
+    return s.partition(delim)[0]
+
+def substring_after(s, delim):
+    return s.partition(delim)[2]
+
+_result_filename_arr = [
+                "results/Nvidia/GeForce_RTX_2060_MaxQ/GeForce_RTX_2060_MaxQ__half_model_train_benchmark.csv",
+                "results/Nvidia/GeForce_GTX_1080_TI/GeForce_GTX_1080_TI__half_model_training_benchmark.csv",
+                "results/AMD/AMD_Radeon_RX_6800/AMD_Radeon_RX_6800__half_model_train_benchmark.csv",
+                "results/Nvidia/TITAN_RTX/TITAN_RTX__half_model_train_benchmark.csv",
+                "results/Nvidia/GeForce_RTX_3090/GeForce_RTX_3090__half_model_train_benchmark.csv",
+                "results/Nvidia/A100_SXM4_40GB/A100_SXM4_40GB__half_model_train_benchmark.csv",
+                "results/AMD/AMD_Radeon_RX_7900_XTX/AMD_Radeon_RX_7900_XTX__half_model_train_benchmark.csv",
+            ]
+"""
+_result_filename_arr = [
+                "test1.csv",
+                "test2.csv",
+            ]
+"""
+### Filter file name list for files ending with .csv
+_result_filename_arr = [file for file in _result_filename_arr if '.csv' in file]
+
+_benchmark_name_arr = ["resnet", "resnext", "densenet", "squeezenet", "vgg", "mobilenet"]
+_benchmark_values_dict = {}
+
+_df_means_appended = []
+basename_arr = []
+### Loop over all files
+for result_filename in _result_filename_arr:
+    ### Read .csv file and append to list
+    df = pd.read_csv(result_filename)
+
+    basename = os.path.basename(result_filename)
+    basename = substring_before(basename, "__")
+    basename_arr.append(basename)
+    
+    tittle_str = substring_after(result_filename, "__")
+    tittle_str = substring_before(tittle_str, "_benchmark")
+
+    # split benchmarks for each device to multiple result arrays
+    # and then collect resutls for multiple gpus to same arrays
+    for benchmark_name in _benchmark_name_arr:
+        _benchmark_values_dict[benchmark_name] = _benchmark_values_dict.get(benchmark_name, [])
+        _df_means_appended = _benchmark_values_dict[benchmark_name]
+        cols = [cc for cc in df.columns if cc.startswith(benchmark_name)]
+        df2 = df[cols]
+        means = df2.mean()
+        _df_means_appended.append(means)
+
+for benchmark_name in _benchmark_name_arr:
+    _df_means_appended = _benchmark_values_dict.get(benchmark_name, [])    
+    df_mean = pd.concat(_df_means_appended, axis=1)
+    df_mean.columns = basename_arr
+    df_mean.plot(xlabel='benchmark model', ylabel='execution time (msec), smaller value is better', title=tittle_str)
+    #plt.plot(df_mean)
+    ### Generate the plot
+    plt.show()
---
+++ show_benchmark_results.sh
@@ -0,0 +1,1 @@
+python3 plot_benchmark_graphs.py
