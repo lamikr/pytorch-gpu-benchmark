cce0e76b670b02ae18ce7e836d1d4460cc96113d
Mika Laitio <lamikr@gmail.com>
2024-08-11 02:20:20 -0700

    sort columns by the number that follows the string
    - fixes the problem where the column
      resnet101 was by default before the resnet34 column
    
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- plot_benchmark_results.py
+++ plot_benchmark_results.py
@@ -1,4 +1,5 @@
 import os
+import re 
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -8,21 +9,26 @@ def substring_before(s, delim):
 def substring_after(s, delim):
     return s.partition(delim)[2]
 
+def num_sort(test_string):
+    return list(map(int, re.findall(r'\d+', test_string)))[0]
+
 #"results/AMD/AMD_Radeon_780M/AMD_Radeon_780M__half_model_train_benchmark.csv",
 #"results/Nvidia/TITAN_RTX/TITAN_RTX__half_model_train_benchmark.csv",
 #"results/Nvidia/GeForce_GTX_1080_TI/GeForce_GTX_1080_TI__half_model_training_benchmark.csv",
 #"results/Nvidia/GeForce_RTX_2080_TI/GeForce_RTX_2080_TI__half_model_training_benchmark.csv",
 #"results/AMD/AMD_Radeon_780M/AMD_Radeon_780M__half_model_train_benchmark.csv",
 
+#                 "results/Nvidia/GeForce_RTX_2060_MaxQ/GeForce_RTX_2060_MaxQ__half_model_train_benchmark.csv",
 _result_filename_arr = [
                 "results/Nvidia/GeForce_GTX_1080_TI/GeForce_GTX_1080_TI__half_model_training_benchmark.csv",
-                "results/Nvidia/GeForce_RTX_2060_MaxQ/GeForce_RTX_2060_MaxQ__half_model_train_benchmark.csv",
+                "results/AMD/AMD_Radeon_RX_7700S_NO_OPT/AMD_Radeon_RX_7700S_NO_OPT__half_model_train_benchmark.csv",
                 "results/AMD/AMD_Radeon_RX_7700S/AMD_Radeon_RX_7700S__half_model_train_benchmark.csv",
                 "results/AMD/AMD_Radeon_RX_6800/AMD_Radeon_RX_6800__half_model_train_benchmark.csv",
                 "results/Nvidia/GeForce_RTX_3090/GeForce_RTX_3090__half_model_train_benchmark.csv",
-                "results/Nvidia/A100_SXM4_40GB/A100_SXM4_40GB__half_model_train_benchmark.csv",
                 "results/AMD/AMD_Radeon_RX_7900_XTX/AMD_Radeon_RX_7900_XTX__half_model_train_benchmark.csv",
             ]
+#                "results/Nvidia/A100_SXM4_40GB/A100_SXM4_40GB__half_model_train_benchmark.csv",
+
 """
 _result_filename_arr = [
                 "test1.csv",
@@ -55,6 +61,7 @@ for result_filename in _result_filename_arr:
         _benchmark_values_dict[benchmark_name] = _benchmark_values_dict.get(benchmark_name, [])
         _df_means_appended = _benchmark_values_dict[benchmark_name]
         cols = [cc for cc in df.columns if cc.startswith(benchmark_name)]
+        cols.sort(key=num_sort) 
         df2 = df[cols]
         means = df2.mean()
         _df_means_appended.append(means)
